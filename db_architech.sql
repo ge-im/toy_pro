489e83c7a904e53ebd886249b20eec2c097cae27
ge-im <myyou1239@gmail.com>
2026-02-12 13:25:53 +0900

    [fix] auth관련 db 변경 1. user_pswd 사이즈 변경(200 -> 255) 2. t_auth -> t_role로 변경 3. 관계테이블에서 연계 컬럼 sn으로 변경

--- db_architech.sql
+++ db_architech.sql
@@ -8,26 +8,25 @@ dev_toy_schema01: 개발자가 작업할 수 있는 구역
 
 CREATE TABLE dev_toy_schema01.t_user_m01 (
 	user_sn SERIAL NOT NULL PRIMARY KEY
-	, user_id VARCHAR(20) NOT NULL 
+	, user_id VARCHAR(20) NOT NULL
 	, user_nm VARCHAR(100) 
-	, user_pswd VARCHAR(200) 
-	, del_yn CHAR(1) 
-	, reg_dt TIMESTAMP 
+	, user_pswd VARCHAR(255) 
+	, del_yn CHAR(1) DEFAULT 'N'
+	, reg_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	, updt_dt TIMESTAMP
 );
 
-CREATE TABLE dev_toy_schema01.t_auth_m01 (
-	auth_sn SERIAL NOT NULL PRIMARY KEY
-	, auth_cd VARCHAR(20) NOT NULL
-	, auth_nm VARCHAR(200)
+CREATE TABLE dev_toy_schema01.t_role_m01 (
+	role_sn SERIAL PRIMARY KEY
+	, role_cd VARCHAR(30) NOT NULL
+	, role_nm VARCHAR(100)
 	, up_auth_sn INTEGER
 );
 
---sn or cd/id? 뭘로 해야하나?
-CREATE TABLE dev_toy_schema01.t_user_auth_s01 (
-	sn SERIAL NOT NULL PRIMARY KEY
-	, user_id INTEGER
-	, auth_cd INTEGER
+CREATE TABLE dev_toy_schema01.t_user_role_s01 (
+	user_role_sn SERIAL NOT NULL PRIMARY KEY
+	, user_sn INTEGER
+	, role_sn INTEGER
 );
 
 CREATE TABLE dev_toy_schema01.t_post_m01 (
@@ -69,6 +68,14 @@ CREATE TABLE dev_toy_schema01.t_like_h01 (
 	, reg_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE dev_toy_schema01.t_authur_h01 (
+	sn SERIAL NOT NULL PRIMARY KEY
+	, target_type CHAR(1) --P:(POST), C(COMMENT)
+	, target_sn INTEGER --POST_SN, COMMENT_SN
+	, user_sn INTEGER
+	, action_type CHAR(1) --A(add: 좋아요), D:(delete: 좋아요 취소-물리삭제)
+	, reg_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
 
